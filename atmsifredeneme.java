82a8d3d74000c889806618f2aded9bb2627954f2
Porders54 <ahmet04tahsin@gmail.com>
2026-07-08 14:21:08 +0300

    Add files via upload

---
+++ atmsifredeneme.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class atmsifredeneme {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int bakiye = 10000 ;
+                boolean atmcalisiyormu = true;
+                        int dogrusifre = 1234 ;
+                        boolean girisbasarilimi = false;
+                        int kalanhak = 3;
+        System.out.println("Deka Banka Hoş Geldiniz");
+        while (girisbasarilimi == false && kalanhak > 0 ) {
+            System.out.println("Lütfen Şifrenizi Giriniz = " + kalanhak);
+            int girilensifre = x.nextInt();
+            if (girilensifre == dogrusifre) {
+                System.out.println("Şifre Doğru, Giriş Yapılıyor");
+                girisbasarilimi = true;
+            } else {
+                kalanhak = kalanhak - 1;
+                if (kalanhak > 0) {
+                    System.out.println("Hatalı Şifre, Lütfen Tekrar Deneyiniz");
+                } else {
+                    System.out.println("3 Defa Yanlış Deneme yaptınız,Kartınız Bloke Edilmiştir");
+                }
+
+            }
+        }
+        if (girisbasarilimi) {
+            while (atmcalisiyormu) {
+                System.out.println("Lütfen İstediğiniz İşlemi Seçin");
+                System.out.println("Bakiye Sorgulama");
+                System.out.println("Para Çekme");
+                System.out.println("Para Yatırma");
+                System.out.println("Çık");
+                int secim = x.nextInt();
+   switch (secim) {
+       case 1:
+           System.out.println("Güncel Bakiye +   = " + bakiye + "TL" );
+           break;
+       case 2:
+           System.out.println("Çekilecek Tutar");
+           int cekilecektutar = x.nextInt();
+              if (cekilecektutar>bakiye)  {
+                  System.out.println("Yetersiz Bakiye Lütfen Tekrar Deneyiniz");
+              } else {
+                  bakiye = bakiye - cekilecektutar;
+                  System.out.println("Yeni Bakiye = " +  bakiye + "TL");
+                  System.out.println("Lütfen Paranızı Alınız");
+              } break;
+       case 3:
+           System.out.println("Yatırılacak Tutar");
+           int yatiricalaktutar = x.nextInt();
+           bakiye = bakiye + yatiricalaktutar;
+           System.out.println("Yeni Bakiye = " + bakiye + "TL");
+           break;
+       case 4:
+           System.out.println("Kartınızı Alınız");
+           atmcalisiyormu = false;
+                   break;
+       default:
+           System.out.println("Yanlış Rakam Girdiniz, Lütfen Tekarar Deneyiniz");
+}
+            }
+        } else {
+            System.out.println("Giriş başarısız olduğu için ATM menüsüne bağlanılamadı");
+        }
+    }
+}
