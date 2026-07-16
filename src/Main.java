1c66a0d3c6299d90ea79f7037dfee7d7a8615953
Ahmet Tahsin Kalkan <ahmet04tahsin@gmail.com>
2026-07-16 12:08:24 +0300

    Refactor: Rename Scanner variable 'x' to 'scanner'

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,68 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        int bakiye = 10000 ;
+        boolean atmcalisiyormu = true;
+        int dogrusifre = 1234 ;
+        boolean girisbasarilimi = false;
+        int kalanhak = 3;
+        System.out.println("Deka Banka Hoş Geldiniz");
+        while (girisbasarilimi == false && kalanhak > 0 ) {
+            System.out.println("Lütfen Şifrenizi Giriniz = " + kalanhak);
+            int girilensifre = scanner.nextInt();
+            if (girilensifre == dogrusifre) {
+                System.out.println("Şifre Doğru, Giriş Yapılıyor");
+                girisbasarilimi = true;
+            } else {
+                kalanhak = kalanhak - 1;
+                if (kalanhak > 0) {
+                    System.out.println("Hatalı Şifre, Lütfen Tekrar Deneyiniz");
+                } else {
+                    System.out.println("3 Defa Yanlış Deneme yaptınız,Kartınız Bloke Edilmiştir");
+                }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            }
+        }
+        if (girisbasarilimi) {
+            while (atmcalisiyormu) {
+                System.out.println("Lütfen İstediğiniz İşlemi Seçin");
+                System.out.println("1- Bakiye Sorgulama");
+                System.out.println("2- Para Çekme");
+                System.out.println("3- Para Yatırma");
+                System.out.println("4- Çık");
+                int secim = scanner.nextInt();
+                switch (secim) {
+                    case 1:
+                        System.out.println("Güncel Bakiye +   = " + bakiye + "TL" );
+                        break;
+                    case 2:
+                        System.out.println("Çekilecek Tutar");
+                        int cekilecektutar = scanner.nextInt();
+                        if (cekilecektutar>bakiye)  {
+                            System.out.println("Yetersiz Bakiye Lütfen Tekrar Deneyiniz");
+                        } else {
+                            bakiye = bakiye - cekilecektutar;
+                            System.out.println("Yeni Bakiye = " +  bakiye + "TL");
+                            System.out.println("Lütfen Paranızı Alınız");
+                        } break;
+                    case 3:
+                        System.out.println("Yatırılacak Tutar");
+                        int yatiricalaktutar = scanner.nextInt();
+                        bakiye = bakiye + yatiricalaktutar;
+                        System.out.println("Yeni Bakiye = " + bakiye + "TL");
+                        break;
+                    case 4:
+                        System.out.println("Kartınızı Alınız");
+                        atmcalisiyormu = false;
+                        break;
+                    default:
+                        System.out.println("Yanlış Rakam Girdiniz, Lütfen Tekarar Deneyiniz");
+                }
+            }
+        } else {
+            System.out.println("Giriş başarısız olduğu için ATM menüsüne bağlanılamadı");
         }
     }
 }
