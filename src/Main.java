39a7b605ceaea1cb964b8f1d9cb508f95b2df5f8
Ahmet Tahsin Kalkan <ahmet04tahsin@gmail.com>
2026-07-16 16:41:00 +0300

    Prevented negative values and non-integer inputs

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,18 @@ public class Main {
         boolean isAtmRunning = true;
         int correctPassword = 1234 ;
         boolean isLoginSuccessful = false;
-        int remaningAttempts = 3;
-        System.out.println("Welcom to Deka Bank");
-        while (isLoginSuccessful == false && remaningAttempts > 0 ) {
-            System.out.println("Pleas Enter Your Password = " + remaningAttempts);
-            int enterPassword = scanner.nextInt();
+        int remainingAttempts = 3;
+        System.out.println("Welcome to Deka Bank");
+        while (!isLoginSuccessful && remainingAttempts > 0 ) {
+            System.out.println("Please Enter Your Password = " + remainingAttempts);
+            int enterPassword = SafeInt(scanner);
             if (enterPassword == correctPassword) {
                 System.out.println("Password Correct, Logging in");
                 isLoginSuccessful = true;
             } else {
-                remaningAttempts = remaningAttempts - 1;
-                if (remaningAttempts > 0) {
-                    System.out.println("Incorrect Password, Pleas Try Again");
+                remainingAttempts = remainingAttempts - 1;
+                if (remainingAttempts > 0) {
+                    System.out.println("Incorrect Password, Please Try Again");
                 } else {
                     System.out.println("3 Incorrect Attempts! Your Card Has Been Blocked");
                 }
@@ -27,19 +27,19 @@ public class Main {
         }
         if (isLoginSuccessful) {
             while (isAtmRunning) {
-                System.out.println("Pleas Select an Operation");
+                System.out.println("Please Select an Operation");
                 System.out.println("1- Balance Inquiry");
                 System.out.println("2- Withdraw Money");
                 System.out.println("3- Deposit Money");
                 System.out.println("4- Exit");
-                int choice = scanner.nextInt();
+                int choice = SafeInt(scanner);
                 switch (choice) {
                     case 1:
-                        System.out.println("Current Balance +   = " + balance + "TL" );
+                        System.out.println("Current Balance  = " + balance + "TL" );
                         break;
                     case 2:
                         System.out.println("Enter Amount to Withdraw");
-                        int withdrawAmount = scanner.nextInt();
+                        int withdrawAmount = SafeInt(scanner);
                         if (withdrawAmount>balance)  {
                             System.out.println("Insufficient Balance. Please try again");
                         } else {
@@ -49,7 +49,7 @@ public class Main {
                         } break;
                     case 3:
                         System.out.println("Enter Amount to Deposit");
-                        int depositAmount = scanner.nextInt();
+                        int depositAmount = SafeInt(scanner);
                         balance = balance + depositAmount;
                         System.out.println("New Balance = " + balance + "TL");
                         break;
@@ -64,5 +64,22 @@ public class Main {
         } else {
             System.out.println("Login failed. Unable to connect to ATM services");
         }
+    }
+    public static int SafeInt(Scanner scanner) {
+        int input = -1 ;
+        boolean isValid = false;
+        while (!isValid) {
+while (!scanner.hasNextInt()) {
+    System.out.println("Error: Please enter numbers only, letters are not allowed");
+    scanner.next();
+}
+input = scanner.nextInt();
+if (input <= 0) {
+    System.out.println("Error: Please enter a value greater than 0");
+} else {
+    isValid = true;
+}
+        }
+        return input;
     }
 }
